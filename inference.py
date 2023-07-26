731efaa374cca996f0fac409a76097dff48660aa
Mathieu <mathieu.nalpon@student-cs.fr>
2023-07-26 15:12:14 +0200

    update inference file

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
-from segment_anything import build_sam
-sam = build_sam.build_sam_vit_h(checkpoint="sam_vit_h_4b8939.pth")
+from segment_anything.build_sam import build_sam_vit_h
+sam =build_sam_vit_h(checkpoint="sam_vit_h_4b8939.pth")
 
 for name, param in sam.named_parameters():
     print(name, param.shape)
