44a77ab9d7b0ef2af54b1feb8f8616578ad331ab
Mathieu <mathieu.nalpon@student-cs.fr>
2023-07-26 15:57:04 +0200

    update inference file

--- inference.py
+++ inference.py
@@ -1,5 +1,18 @@
 from segment_anything.build_sam import build_sam_vit_h
-sam =build_sam_vit_h(checkpoint="sam_vit_h_4b8939.pth")
+from segment_anything import SamPredictor, SamAutomaticMaskGenerator
+from PIL import Image
+import requests
 
-for name, param in sam.named_parameters():
-    print(name, param.shape)
+# load model and load weights
+sam = build_sam_vit_h(checkpoint="sam_vit_h_4b8939.pth")
+sam_generator = SamAutomaticMaskGenerator(sam) 
+
+
+# load image
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+
+sam_generator.set_image(image)
+
+sam_generator.predict(image)
