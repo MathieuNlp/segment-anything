4c9244fd14d9d2852a8700d4cfa3698a2c6b8cab
Mathieu <mathieu.nalpon@student-cs.fr>
2023-07-26 16:01:26 +0200

    update inference file

--- inference.py
+++ inference.py
@@ -2,7 +2,23 @@ from segment_anything.build_sam import build_sam_vit_h
 from segment_anything import SamPredictor, SamAutomaticMaskGenerator
 from PIL import Image
 import requests
+import matplotlib.pyplot as plt
 
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+    
 # load model and load weights
 sam = build_sam_vit_h(checkpoint="sam_vit_h_4b8939.pth")
 sam_generator = SamAutomaticMaskGenerator(sam) 
@@ -12,7 +28,12 @@ sam_generator = SamAutomaticMaskGenerator(sam)
 url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
 image = Image.open(requests.get(url, stream=True).raw)
 
+masks = sam_generator.generate(image)
 
-sam_generator.set_image(image)
+# plots masks
 
-sam_generator.predict(image)
+plt.figure(figsize=(20,20))
+plt.imshow(image)
+show_anns(masks)
+plt.axis('off')
+plt.show() 
